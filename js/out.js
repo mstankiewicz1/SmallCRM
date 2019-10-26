584a78046923efd5371bee00431f4fe6333ce918
Marek Stankiewicz <marekstankiewicz6@wp.pl>
2019-10-26 23:35:47 +0200

    Postepy 26.10.19r godz 23:5

--- js/out.js
+++ js/out.js
@@ -22891,7 +22891,7 @@ var Order = function (_React$Component) {
             var _this2 = this;
 
             var orderedBooks = this.props.order.map(function (order) {
-                return _react2.default.createElement(_OrderView2.default, { key: order.name, book: order, removeFromOrder: _this2.props.removeFromOrder });
+                return _react2.default.createElement(_OrderView2.default, { book: order, removeFromOrder: _this2.props.removeFromOrder });
             });
 
             return _react2.default.createElement(
